d28af6cc0207fa35674c8c01d3b440a093c60fd3
Justus Winter <justus@sequoia-pgp.org>
2020-08-19 16:14:06 +0200

    Add more test vectors.

--- src/tests/certificates/perturbed_certs.rs
+++ src/tests/certificates/perturbed_certs.rs
@@ -1,9 +1,10 @@
 use sequoia_openpgp as openpgp;
 use openpgp::{
     Packet,
-    packet::Tag,
+    packet::{key, Key, Tag},
     parse::Parse,
-    serialize::Serialize,
+    serialize::{Serialize, SerializeInto},
+    types::SignatureType,
 };
 
 use crate::{
@@ -49,7 +50,11 @@ impl Test for PerturbedCerts {
         \
         <p>Notation: <b>P</b>rimary key, <b>U</b>serID, <b>U</b>serID \
         <b>B</b>inding, <b>S</b>ubkey, <b>S</b>ubkey <b>B</b>inding, \
-        <b>M</b>arker.<b>U*</b>nbound UserID.</p>"
+        <b>M</b>arker, <b>U*</b>nbound UserID, unbound \
+        <b>S*</b>ubkey, <b>B</b>ad signature, <b>O</b>dd signature, \
+        <b>S</b>ubkey version<b>23</b>, \
+        <b>S</b>ubkey <b>B</b>inding version<b>23</b>, \
+        <b>X</b>tremely unknown packet type.</p>"
             .into()
     }
 
@@ -98,6 +103,41 @@ impl ConsumerTest for PerturbedCerts {
         let markerp = openpgp::Packet::Marker(Default::default());
         let marker = &markerp;
 
+        let subkey_unbound: openpgp::Packet =
+            Key::<key::PublicParts, key::SubordinateRole>::from(
+                key::Key4::generate_rsa(2048)?.parts_into_public()).into();
+
+        // Make a bad binding signature.
+        let mut buf = subkeyb.to_vec()?;
+        let l = buf.len();
+        buf[l - 2] ^= 0xff;
+        let bad_sig = openpgp::Packet::from_bytes(&buf)?;
+        assert_eq!(bad_sig.kind(), Some(Tag::Signature));
+
+        // Make an odd signature.
+        buf[4] = SignatureType::Binary.into();
+        let odd_sig = openpgp::Packet::from_bytes(&buf)?;
+        assert_eq!(odd_sig.kind(), Some(Tag::Signature));
+
+        // Make a fictitious new signature.
+        buf[3] = 23;
+        let fictitious_sig = openpgp::Packet::from_bytes(&buf)?;
+        assert_eq!(fictitious_sig.tag(), Tag::Signature);
+
+        // Make a fictitious new key.
+        let mut buf = subkey.to_vec()?;
+        buf[3] = 23;
+        let fictitious_subkey = openpgp::Packet::from_bytes(&buf)?;
+        assert_eq!(fictitious_subkey.tag(), Tag::PublicSubkey);
+
+        // Make a fictitious new component.
+        let mut unknown = openpgp::packet::Unknown::new(Tag::Unknown(59),
+                                                        anyhow::anyhow!(""));
+        let mut bullshit = vec![0; 659];
+        openpgp::crypto::random(&mut bullshit);
+        unknown.set_body(bullshit);
+        let unknown = Packet::from(unknown);
+
         Ok(vec![
             make("P U UB S SB",
                  vec![primary, uid, uidb, subkey, subkeyb],
@@ -111,6 +151,9 @@ impl ConsumerTest for PerturbedCerts {
             make("P U UB U S SB",
                  vec![primary, uid, uidb, uid, subkey, subkeyb],
                  Some(Ok("Duplicated UserID".into())))?,
+            make("P U UB UB S SB",
+                 vec![primary, uid, uidb, uidb, subkey, subkeyb],
+                 Some(Ok("Duplicated UserID binding".into())))?,
             make("P U UB S SB S SB",
                  vec![primary, uid, uidb, subkey, subkeyb, subkey, subkeyb],
                  Some(Ok("Duplicated subkey".into())))?,
@@ -120,6 +163,9 @@ impl ConsumerTest for PerturbedCerts {
             make("P U UB S SB S",
                  vec![primary, uid, uidb, subkey, subkeyb, subkey],
                  Some(Ok("Duplicated subkey".into())))?,
+            make("P U UB S SB SB",
+                 vec![primary, uid, uidb, subkey, subkeyb, subkeyb],
+                 Some(Ok("Duplicated subkey binding".into())))?,
             make("P U UB S",
                  vec![primary, uid, uidb, subkey],
                  Some(Err("Subkey not bound".into())))?,
@@ -132,6 +178,9 @@ impl ConsumerTest for PerturbedCerts {
             make("P U UB U* S SB",
                  vec![primary, uid, uidb, uid_unbound, subkey, subkeyb],
                  Some(Ok("Unbound UserID should be ignored".into())))?,
+            make("P U UB S SB S*",
+                 vec![primary, uid, uidb, subkey, subkeyb, &subkey_unbound],
+                 Some(Ok("Unbound subkey should be ignored".into())))?,
             make("P M U UB S SB",
                  vec![primary, marker, uid, uidb, subkey, subkeyb],
                  Some(Ok("Marker packet MUST be ignored".into())))?,
@@ -147,6 +196,22 @@ impl ConsumerTest for PerturbedCerts {
             make("P UB SB U S",
                  vec![primary, uidb, subkeyb, uid, subkey],
                  None)?,
+            make("P U UB S SB B",
+                 vec![primary, uid, uidb, subkey, subkeyb, &bad_sig],
+                 Some(Ok("Bad signature should be ignored".into())))?,
+            make("P U UB S SB O",
+                 vec![primary, uid, uidb, subkey, subkeyb, &odd_sig],
+                 Some(Ok("Bad signature should be ignored".into())))?,
+            make("P U UB S SB SB23",
+                 vec![primary, uid, uidb, subkey, subkeyb, &fictitious_sig],
+                 Some(Ok("Unknown signature version should be ignored".into())))?,
+            make("P U UB S SB S23 B",
+                 vec![primary, uid, uidb, subkey, subkeyb,
+                      &fictitious_subkey, &bad_sig],
+                 Some(Ok("Unknown key version should be ignored".into())))?,
+            make("P U UB S SB X B",
+                 vec![primary, uid, uidb, subkey, subkeyb, &unknown, &bad_sig],
+                 Some(Ok("Unknown components should be ignored".into())))?,
         ])
     }
 
